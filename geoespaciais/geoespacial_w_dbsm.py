9a12eb2ef818d6b113bc65a7b95e6681d45d9493
MarksMestre <marcoslimamestre@gmail.com>
2026-06-14 11:36:59 +0100

    alt geoespacial

--- geoespaciais/geoespacial_w_dbsm.py
+++ geoespaciais/geoespacial_w_dbsm.py
@@ -2,9 +2,21 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 import sqlite3
 import pandas as pd
+from dask.dataframe import to_csv
+
+
+def vector_map(geojson_directory):
+    districts_gdf = gpd.read_file(geojson_directory)  # Ficheiro com mapa de distritos, já está em formato WGS84
+    districts_gdf = districts_gdf[~districts_gdf['name'].isin(['Azores', 'Madeira'])]
+    print(districts_gdf.geometry.head())
+    portugal_geom = [districts_gdf.union_all().buffer(0.1)]
+    print("Função Vector_map completada")
+    district_names_series = districts_gdf['name']
+    return districts_gdf, portugal_geom, district_names_series
 
 
 def main():
+
     file_name = 'dbsm-portugal-R2025.gpkg'
     layer_name = 'dbsm-portugal-R2025'
 
@@ -46,34 +58,22 @@ def main():
     df_stats = pd.read_sql_query(query, conn)
     conn.close()
 
-    # Print results with formatting
+
     total_mwh = df_stats['total_mwh_year'][0]
     print(f"--- Solar Potential Summary ---")
     print(f"Total Buildings Analyzed: {df_stats['total_buildings'][0]:,}")
     print(f"Total Solar Potential:    {total_mwh:,.2f} MWh/year")
     print(f"Average per Building:     {df_stats['avg_mwh_year'][0]:.2f} MWh/year")
+    print(f"In Terawatt-hours:        {total_mwh / 1_000_000:.2f} TWh/year")
 
-    # Contextualizing the number:
-    # 1 GWh = 1,000 MWh. 1 TWh = 1,000,000 MWh.
-    if total_mwh > 1_000_000:
-        print(f"In Terawatt-hours:        {total_mwh / 1_000_000:.2f} TWh/year")
 
-    def vector_map(geojson_directory):
-        districts_gdf = gpd.read_file(geojson_directory)  # Ficheiro com mapa de distritos, já está em formato WGS84
-        districts_gdf = districts_gdf[~districts_gdf['name'].isin(['Azores', 'Madeira'])]
-        print(districts_gdf.geometry.head())
-        portugal_geom = [districts_gdf.union_all().buffer(0.1)]
-        print("Função Vector_map completada")
-        district_names_series = districts_gdf['name']
-        return districts_gdf, portugal_geom, district_names_series
 
     districts_gdf, portugal_geom, district_names_series = vector_map("pt.json")
 
 
 
 
-    # 2. Ensure CRS consistency
-    # If your vector_map returns WGS84, the buildings must be WGS84 too
+    #Consistência CRS
     if gdf.crs != districts_gdf.crs:
         print("CRS mismatch", districts_gdf.crs, gdf.crs)
         gdf = gdf.to_crs(districts_gdf.crs)
@@ -84,7 +84,6 @@ def main():
     #Soma por distrito
     district_potential = joined_gdf.groupby('name')['rooftop_pv_potential_MWh_per_y'].sum().reset_index()
 
-    # 5. Format and Print Results
     district_potential['TWh_per_year'] = district_potential['rooftop_pv_potential_MWh_per_y'] / 1_000_000
     print(district_potential[['name', 'TWh_per_year']].sort_values(by='TWh_per_year', ascending=False))
     district_potential = district_potential.rename(columns={'name': 'Distrito'})
